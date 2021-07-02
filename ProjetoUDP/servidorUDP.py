ffd84d762520acf9a2fe9d3ec8c2cb04d7fa56f2
danielturmina <54449053+danielturmina@users.noreply.github.com>
2021-07-02 11:20:28 -0300

    Update servidorUDP.py

--- ProjetoUDP/servidorUDP.py
+++ ProjetoUDP/servidorUDP.py
@@ -12,33 +12,63 @@ def lerArquivo(arquivo):
     cont = 0
     for linha in arquivo:
         linha = linha.strip()
-        if cont <= 1:
-            lista.append(linha)
-            cont += 1
-        else:
-            perguntas.append(lista)
-            lista = []
-            cont = 0
-            lista.append(linha)
-            cont += 1
-    perguntas.append(lista)        
+        lista.append(linha)
+    while cont+1 <= len(lista):
+        tupla = (lista[cont],lista[cont+1])
+        perguntas.append(tupla)
+        cont += 2      
     return perguntas
 
-def receber_dados():
+def enviar_dados(msg, cliente_endereco):
+    servidor_socket.sendto(msg.encode(),cliente_endereco)
 
+def iniciarPartida():
+    global comecou
+    comecou = True
+    rodada = []
+    while len(rodada) < 5:
+        escolher = random.choice(perguntas)
+        rodada.append(escolher)
+    print(rodada)
+    
+
+def receber_dados():
     while True:
         dados, cliente_endereco = servidor_socket.recvfrom(2048)    
         dados = dados.decode()
-        print(dados)
-        print(cliente_endereco)
+        print(f"Servidor recebeu: {dados} do cliente: {cliente_endereco}")
+        if len(listaJogadores) < 5 and cliente_endereco not in listaClientes and comecou == False:
+            listaJogadores.append([dados,cliente_endereco])
+            listaClientes.append(cliente_endereco)
+            enviar_dados("Digite 'start' para começar",cliente_endereco)
+            print(listaPontuacao)
+            print(listaJogadores)
+        elif cliente_endereco not in listaClientes and comecou == True:
+            enviar_dados("Você não poderá participar do Jogo pois a Partida já começou! Jogo Encerrado!",cliente_endereco)
+        elif cliente_endereco not in listaClientes and len(listaJogadores) >= 5:
+            enviar_dados("Você não poderá participar do Jogo pois já atingimos o limite de Participantes! Jogo Encerrado!",cliente_endereco)
+        elif dados == "start"and cliente_endereco not in preparados:
+            if comecou == False:
+                preparados.append(cliente_endereco)
+                listaPontuacao.append([cliente_endereco,0])
+                enviar_dados("A partida iniciará em breve!",cliente_endereco)
+                if len(preparados)>=2:
+                    time.sleep(30)
+                    iniciarPartida()
+            else:
+                enviar_dados("Você não deu start a tempo, a partida já começou",cliente_endereco)
 
-while True:
-    Thread(target=receber_dados).start()
+         
+listaJogadores = []
+listaClientes = []
+listaPontuacao = []
+qnt = 0
+preparados = []
+comecou = False
 
-
-
-#teste
 arquivo = open("campeoes.txt", "r")
 perguntas = lerArquivo(arquivo)
 arquivo.close()
 
+while True:
+    Thread(target=receber_dados).start()
