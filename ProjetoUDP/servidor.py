415ccb38474aaab3b482f5ecd3fcf0477a750c51
Yuri Barros <50000444+yuricdb@users.noreply.github.com>
2021-07-03 21:38:26 -0300

    Update servidor.py

--- ProjetoUDP/servidor.py
+++ ProjetoUDP/servidor.py
@@ -4,8 +4,7 @@ import random
 
 #variaveis
 jogadores = []
-
-
+numRodada = 0
 
 #cria servidor
 servidor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -26,12 +25,10 @@ def lerArquivo(arquivo):
         cont += 2      
     return perguntas
 
-
-arquivo = open('C:\\Users\\YNC\\Documents\\codigosEstudos\\campeoes.txt', "r")  
+arquivo = open('C:\\Users\\YNC\\Documents\\codigosEstudos\\campeoes.txt', "r") 
 perguntas = lerArquivo(arquivo)
 arquivo.close()
 #####
-
 
 perguntaSortida = [] #lista com 5 tuplas de perguntas e respostas
 
@@ -40,21 +37,34 @@ for i in sorteio: #add 5 tuplas de perguntas e respostas a uma lista
     perguntaSortida.append(perguntas[int(i)]) 
 
 print(perguntaSortida) #verificando se deu certo
-input('') #debugando
 
 
 def enviaTodos(lista, msg): #envia msg a todos
     for i in lista:
         servidor.sendto(msg.encode().upper(), i)
 
+def verificaResp(msg, end):
+    if msg.upper() == perguntaSortida[0][1].upper():
+        enviaTodos(jogadores, f'Jogador {end} acertou\n')
+    else:
+        print(f'jogador {end} perdeu tanto ponto\n')
+
+
+#muita coisa abaixo tem que virar função e tbm organizar melhor
 def loop():
     while True:
         msgBytes, endCliente = servidor.recvfrom(2048) 
         msgBytes = msgBytes.decode()
         if endCliente not in jogadores:
             jogadores.append(endCliente)
-        print(msgBytes, endCliente) #debugando
-        enviaTodos(jogadores, msgBytes)
+        print(jogadores)
+        if len(jogadores) >=3: #aumentar esse numero depios p\ 5, só coloquei 3 pra nao precisar abrir tanto cliente
+            print(msgBytes, endCliente) #debugando
+            enviaTodos(jogadores, msgBytes)
+            msgRodada = 'rodada numero '+ str(numRodada) #depois tirar, só pra teste
+            enviaTodos(jogadores, msgRodada)
+            enviaTodos(jogadores, perguntaSortida[0][0])
+            verificaResp(msgBytes, endCliente)
         #servidor.sendto(msgBytes.encode(), endCliente)
         #print('mandou')
 
