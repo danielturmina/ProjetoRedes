d354c595bf8530e3ee2cef43b7c7b9e93d6c0512
Yuri Barros <50000444+yuricdb@users.noreply.github.com>
2021-07-05 15:54:46 -0300

    Update cliente.py

--- ProjetoUDP/cliente.py
+++ ProjetoUDP/cliente.py
@@ -5,7 +5,7 @@ import time
 encerra = False
 cliente = socket(AF_INET, SOCK_DGRAM)
 
-print('digite seu nome:\n')
+print('digite seu nome:\n') #aparece só uma vez assim que inicia o cliente
 
 def envia():
     global encerra
@@ -21,9 +21,8 @@ def recebe():
         print(msgBytes.decode())
 
 #é preciso ter esse inicio pra o servidor se conectar com cliente primeiramente
-msg = 'Conectado: '#input('nome do jogador\n') 
+msg = 'Conectado: '# manda uma msg automatica assim que inicia o servidor, para que o servidor entendenda que esse usuario conectou
 cliente.sendto(msg.encode(),('localhost',12000))
-#################
 
 Thread(target=recebe).start()
 Thread(target=envia).start()
