6dc08fee0918ab2876efad5745f9140e78ded3f7
Yuri Barros <50000444+yuricdb@users.noreply.github.com>
2021-07-05 14:52:28 -0300

    Update servidor.py

--- ProjetoUDP/servidor.py
+++ ProjetoUDP/servidor.py
@@ -1,17 +1,34 @@
+'''Cada rodada será encerrada quando algum participante acertar a resposta ou atingir
+uma duração máxima de 10 segundos;
+Pontuação de cada rodada: Cada resposta errada: -5 pontos, sem resposta: -1 ponto, Resposta correta: 25 pontos
+Após uma competição ser encerrada, um ranking com a pontuação é divulgado e uma nova competição poderá ser iniciada.'''
+
 import socket 
 from threading import Thread
 import random
+import time
 
-#variaveis
+#testar futuramente
+class Jogadores:
+    def __init__(self):
+        self.pronto = False
+        self.nome = ''
+        self.pontos = 0
+        self.end = ''
+
 jogadores = []
+jogadoresProntos = []
+perguntaSortida = [] 
+comecou = False
+start = True
 numRodada = 0
+porta = 12000
+contTempo = 10 #usei dez segundos pra o criterio q o prof pede, além disso é usado tanto pra iniciar um jogo quanto pra tempo de resposta
+tempoInterrompido = False
 
-#cria servidor
 servidor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-servidor.bind(('',12000))
-####
+servidor.bind(('', porta)) 
 
-#trata o banco de dados em txt
 def lerArquivo(arquivo):
     perguntas = []
     lista = []
@@ -25,49 +42,107 @@ def lerArquivo(arquivo):
         cont += 2      
     return perguntas
 
-arquivo = open('C:\\Users\\YNC\\Documents\\codigosEstudos\\campeoes.txt', "r") 
+arquivo = open('campeoes.txt', "r") 
 perguntas = lerArquivo(arquivo)
 arquivo.close()
-#####
 
-perguntaSortida = [] #lista com 5 tuplas de perguntas e respostas
-
-sorteio = random.sample(range(0,19), 5) #sorteia uma lista com 5 numero sem repetições *mais eficaz
-for i in sorteio: #add 5 tuplas de perguntas e respostas a uma lista
+sorteio = random.sample(range(0,19), 5) 
+for i in sorteio: 
     perguntaSortida.append(perguntas[int(i)]) 
 
-print(perguntaSortida) #verificando se deu certo
-
+print(perguntaSortida) #debug
 
 def enviaTodos(lista, msg): #envia msg a todos
     for i in lista:
-        servidor.sendto(msg.encode().upper(), i)
+        servidor.sendto(msg.encode(), i)
 
-def verificaResp(msg, end):
-    if msg.upper() == perguntaSortida[0][1].upper():
-        enviaTodos(jogadores, f'Jogador {end} acertou\n')
-    else:
-        print(f'jogador {end} perdeu tanto ponto\n')
+def recebe():
+    msgRecebida, endRecebido = servidor.recvfrom(2048) 
+    msgRecebida = msgRecebida.decode()
+    return [msgRecebida, endRecebido]
+
+def cronometro():
+    
+    global contTempo, porta, comecou, tempoInterrompido
+    contTempo = 10 
+    somaCont = 0
+
+    while contTempo > 0:
+        time.sleep(1)
+        print(contTempo)
+        if not comecou:
+            enviaTodos(jogadoresProntos, str(contTempo))
+        somaCont += contTempo
+        contTempo -= 1  
+
+    if (somaCont == 55) and (not comecou):
+        servidor.sendto('o jogo começou!'.encode(),('localhost',porta))
+    elif (somaCont == 55) and comecou:
+        servidor.sendto('o jogo começou!'.encode(),('localhost',porta))
+        
+    tempoInterrompido = True
+    print('cronometro fechado')
 
 #muita coisa abaixo tem que virar função e tbm organizar melhor
-def loop():
-    while True:
-        msgBytes, endCliente = servidor.recvfrom(2048) 
-        msgBytes = msgBytes.decode()
-        if endCliente not in jogadores:
-            jogadores.append(endCliente)
-        print(jogadores)
-        if len(jogadores) >=3: #aumentar esse numero depios p\ 5, só coloquei 3 pra nao precisar abrir tanto cliente
-            print(msgBytes, endCliente) #debugando
-            enviaTodos(jogadores, msgBytes)
-            msgRodada = 'rodada numero '+ str(numRodada) #depois tirar, só pra teste
-            enviaTodos(jogadores, msgRodada)
-            enviaTodos(jogadores, perguntaSortida[0][0])
-            verificaResp(msgBytes, endCliente)
-        #servidor.sendto(msgBytes.encode(), endCliente)
-        #print('mandou')
+def inicio(): #o jogo ainda não começou
+    global contTempo, porta, comecou, tempoInterrompido
 
-Thread(target=loop).start()
+    while True:
+        
+        msgBytes, endCliente = recebe()
+        if msgBytes == 'Conectado: ':
+            if endCliente not in jogadores:
+                jogadores.append(endCliente)
+            enviaTodos([endCliente], 'Digite "start" para começar: \n') #vai enviar só para o que acabou de entrar essa msg (pq é uma lista com um nome só)
+
+        print(msgBytes, endCliente) #debugando
+
+        if (msgBytes == 'start') and (endCliente not in jogadoresProntos): 
+            jogadoresProntos.append(endCliente)
+            enviaTodos([endCliente], 'Sua partida está prestes a começar\n')
+            if len(jogadoresProntos) >= 2:
+                contTempo = 0 #reinicia o cronometro pq esse jogador acabou de entrar
+                time.sleep(1) #tempo de processamento pra a funcao cronometro ser interrompida por completo
+                Thread(target=cronometro).start() #inicia o contador outra vez, atraves dessa thread é possivel que um jogador interrompa em tempo real o cronometro           
+        print(jogadoresProntos) #debug
+        
+        if endCliente[1] == porta: #termina essa parte de esperar jogadores e vai iniciar o jogo.
+            Thread(target=jogando).start()
+            tempoInterrompido = False
+            comecou = True
+            break
+
+def jogando(): #lembrar de impedir que não conseguiu se conectar de responder (apesar de que as perguntas só são enviadas para os jogadoresProntos)
+    print('entrou em jgando')
+    global contTempo, jogadoresProntos, tempoInterrompido, perguntaSortida, numRodada
+  
+    while numRodada < 5:
+
+        enviaTodos(jogadoresProntos, perguntaSortida[numRodada][0])
+        time.sleep(1)
+        Thread(target=cronometro).start()
+
+        while not tempoInterrompido:
+        
+            #print(perguntaSortida[i][1], i)
+            msgBytes, endCliente = recebe()
+            if endCliente[1] == porta:
+                print(' o cronometro zerou e ninguem acertou')
+                break
+
+            print('testando ', msgBytes, ' ', perguntaSortida[numRodada][1])
+            if msgBytes == perguntaSortida[numRodada][1]:
+                enviaTodos(jogadoresProntos, 'endCliente acertou\n')
+                contTempo = 0
+                time.sleep(0.5) #tempo pra processamento
+                
+            elif msgBytes != perguntaSortida[numRodada ][1]:
+                print('diminuir ponto de quem errou\n')
+        
+        numRodada += 1
+        tempoInterrompido = False
+        
+Thread(target=inicio).start()
 
 
 
