61cf83331ff9dd3b5c3351044a14fa4a3deaa93d
danielturmina <54449053+danielturmina@users.noreply.github.com>
2021-07-07 14:51:38 -0300

    Update clienteUDP.py

--- ProjetoUDP/clienteUDP.py
+++ ProjetoUDP/clienteUDP.py
@@ -1,8 +1,8 @@
 from socket import socket, AF_INET, SOCK_DGRAM
 from threading import Thread
-import time
+import time #Não estamos usando
 
-encerra = False
+encerra = False # Qual o uso?
 cliente = socket(AF_INET, SOCK_DGRAM)
 
 
@@ -17,9 +17,13 @@ def envia():
 def recebe():
     while True:
         msgBytes, endServidor = cliente.recvfrom(2048)
-        print(msgBytes.decode())
+        if msgBytes.decode() in dicRespostas:
+            texto = msgBytes.decode()
+            print(dicRespostas[texto])
+        else: 
+            print(msgBytes.decode())
 
-
+print("--- Quiz: Campeões do Mundo! --- ")
 texto = 'Conectado: '
 while True: # Pedindo nome e evitando nomes vazios
     nome = input('Digite o seu nome: \n')
@@ -27,7 +31,15 @@ while True: # Pedindo nome e evitando nomes vazios
         break 
 msg = texto + nome
 cliente.sendto(msg.encode(),('localhost',12000))
- 
+dicRespostas = {
+'100':'Digite "start" para começar:',
+'101':'Sua partida está prestes a começar\n',
+'102':'A quantidade limite de jogadores foi atingida! Tente novamente mais tarde!\n',
+'103':'Infelizmente você não digitou "start" a tempo, a partida já iniciou!\n',
+'104':'Partida já iniciada, volte mais tarde!!\n',
+'200':'Você acertou!',
+'300':'Você errou! Tente novamente...'}
+
 
 Thread(target=recebe).start()
 Thread(target=envia).start()
